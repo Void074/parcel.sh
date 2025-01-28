0c68461d8bd1d7878aaa8377d5008db9d3ba92bd
void074 <devpro074@gmail.com>
2025-01-29 05:59:11 +1000

    initial commit

---
+++ cmds.sh
@@ -0,0 +1,70 @@
+#!/urs/bin/env bash
+## Author: Void074
+## Website: https://gurialabs.fly.dev
+## Git: https://codeberg.org/Void074
+
+declare -A commands=(
+  ["install"]="install_package"
+  ["remove"]="remove_package"
+  ["update"]="update_system"
+  ["--help"]="help"
+)
+
+function help() {
+    echo "getting started"
+    return 0
+}
+
+function install_package() {
+    local cmd=(sudo dnf install "$@")
+   if ! "${cmd[@]}"; then
+        echo "Failed to install: $*" >&2
+        return 1
+    fi
+
+    return 0
+}
+
+function update_system() {
+    local cmd=(sudo dnf update)
+
+		if ! "${cmd[@]}"; then
+			echo "Failed to update system"
+			return 1
+		fi
+
+    return 0
+}
+
+function remove_package() {
+    local cmd=(sudo dnf remove "$@")
+
+		if ! "${cmd[@]}"; then
+			echo "Failed to remove: $*" >&2
+			return 1
+		fi
+
+    return 0
+}
+
+function main() {
+    local cmd="$1"
+
+    if [[ -z "$cmd" ]]; then
+        echo "usage: cmds --help"
+        return 1
+    fi
+
+    echo "cmd -> $cmd"
+    shift || true
+
+    if [[ -n  "${commands[$cmd]}" ]]; then
+        ${commands[$cmd]} "$@"
+    else
+        echo "unknown command: $cmd"
+        echo "usage: cmds --help"
+        return 1
+    fi
+}
+
+main "$@"
