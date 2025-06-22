31aef7bb44b3b2821d3c7e296e76a72864704ee6
void074 <devpro074@gmail.com>
2025-06-22 21:48:37 +1000

    feat:
    - moved commands lib/cmd.sh
    - updated parcel.sh to incorporation change above

---
+++ lib/cmd.sh
@@ -0,0 +1,47 @@
+function _help() {
+    echo "Parcel - A simple cli wrapper around the basic dnf commands"
+    echo "Author: Void074"
+    echo "Repo: https://codeberg.org/Void074"
+    echo "Version: 0.0.0-beta"
+    echo ""
+    echo "Usage: parcel [command]"
+    echo ""
+    echo "Command           Description"
+    echo "install [args]    Install a package or a list of packages"
+    echo "update            Syncs and updates systems"
+    echo "remove [args]     Uninstall a package or list of packages"
+    return 0
+}
+
+function install_package() {
+   local cmd=(sudo dnf install "$@")
+
+   if ! "${cmd[@]}"; then
+        echo "Failed to install: $*" >&2
+        return 1
+    fi
+
+    return 0
+}
+
+function update_system() {
+    local cmd=(sudo dnf update)
+
+	if ! "${cmd[@]}"; then
+		echo "Failed to update system"
+		return 1
+	fi
+
+    return 0
+}
+
+function remove_package() {
+    local cmd=(sudo dnf remove "$@")
+
+	if ! "${cmd[@]}"; then
+		echo "Failed to remove: $*" >&2
+		return 1
+	fi
+
+    return 0
+}
--- parcel.sh
+++ parcel.sh
@@ -3,60 +3,23 @@
 ## Website: https://gurialabs.fly.dev
 ## Git: https://codeberg.org/Void074
 
+##!!!! Enviroment Variables !!!!##
+PROOT=$(dirname $(dirname $(realpath $0)))
+export PROOT
+cmd="$PROOT/parcel/lib/cmd.sh"
+##!!! ------------------------------------------------ !!!!##
+
+##!!!! Source in library, helpers and utility function !!!!##
+source $cmd
+##!!! ------------------------------------------------ !!!!##
+
+##!!!! Main !!!!###
 declare -A commands=(
   ["install"]="install_package"
   ["remove"]="remove_package"
   ["update"]="update_system"
-  ["--help"]="help"
+  ["--help"]="_help"
 )
-
-function help() {
-    echo "Parcel - A simple cli wrapper around the basic dnf commands"
-    echo "Author: Void074"
-    echo "Repo: https://codeberg.org/Void074"
-    echo "Version: 0.0.0-beta"
-    echo ""
-    echo "Usage: parcel [command]"
-    echo ""
-    echo "Command           Description"
-    echo "install [args]    Install a package or a list of packages"
-    echo "update            Syncs and updates systems"
-    echo "remove [args]     Uninstall a package or list of packages"
-    return 0
-}
-
-function install_package() {
-   local cmd=(sudo dnf install "$@")
-
-   if ! "${cmd[@]}"; then
-        echo "Failed to install: $*" >&2
-        return 1
-    fi
-
-    return 0
-}
-
-function update_system() {
-    local cmd=(sudo dnf update)
-
-		if ! "${cmd[@]}"; then
-			echo "Failed to update system"
-			return 1
-		fi
-
-    return 0
-}
-
-function remove_package() {
-    local cmd=(sudo dnf remove "$@")
-
-		if ! "${cmd[@]}"; then
-			echo "Failed to remove: $*" >&2
-			return 1
-		fi
-
-    return 0
-}
 
 function main() {
     local cmd="$1"
